3ffdc5d2b5d794c1dc413cb01a1d7b0dc8735f54
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2024-07-26 09:28:33 +0530

    Update new-add.py

--- new-add.py
+++ new-add.py
@@ -1,10 +1,10 @@
 while True:
     try:
-        first_number = int(input("Enter the first number: "))
-        second_number = int(input("Enter the second number: "))
+        first_number = int(input("Enter the first number (or type exit to quit): "))
+        second_number = int(input("Enter the second number (or type exit to quit): "))
         break
     except ValueError:
         print("Please enter valid numbers.")
 
-sum = first_number + second_number
-print(f"Sum is: {sum}")
+total = first_number + second_number
+print(f"Sum is: {total}")
